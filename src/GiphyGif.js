48edaa1ae5e92046f849cfad69a6a8b1c9002a5d
Jessie A. Morris <jessie@jessieamorris.com>
2019-03-17 18:50:05 -0600

    Fullscreen handling of gifs

--- src/GiphyGif.js
+++ src/GiphyGif.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import Fullscreen from "react-full-screen";
 import './GiphyGif.css';
 
 class GiphyGif extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFullscreenEnabled: false,
+    };
+  }
+
   render() {
     return (
-      <img alt={this.props.gif.title} src={this.props.gif.images.fixed_width.url} />
+      <div class="GiphyGif-wrapper">
+        <img alt={this.props.gif.title} src={this.props.gif.images.fixed_width.url} onClick={() => this.setState({isFullscreenEnabled: true})} />
+
+        <Fullscreen
+          enabled={this.state.isFullscreenEnabled}
+          onChange={isFullscreenEnabled => this.setState({isFullscreenEnabled})}
+        >
+          <img alt={this.props.gif.title} src={this.props.gif.images.original.url} />
+        </Fullscreen>
+      </div>
     );
   }
 }
